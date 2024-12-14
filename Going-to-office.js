9f07675d4c3c4b9672ca155fbbd70aa638a6aa5e
Ahmad Aldali <32056043+ahmadaldali@users.noreply.github.com>
2024-12-14 14:09:12 +0200

    Update Going-to-office.js

--- Going-to-office.js
+++ Going-to-office.js
@@ -1,1 +1,31 @@
-
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+var stdin_input = "";
+ 
+process.stdin.on("data", function (input) {
+    stdin_input += input;
+});
+ 
+process.stdin.on("end", function () {
+   main(stdin_input);
+});
+ 
+function solve(data) {
+    const D = BigInt(data[0]);
+    const [Oc, Of, Od] = data[1].split(" ").map((char)=> BigInt(char));
+    const [Cs, Cb, Cm, Cd] = data[2].split(" ").map((char)=> BigInt(char));
+ 
+    const onlineCost = Oc + (D-Of) * Od;
+    const classicCost = Cb + (D*Cd) + Cm*(BigInt(parseInt(D/Cs)))
+ 
+    if (onlineCost <= classicCost) {
+        console.log("Online Taxi")
+    } else {
+        console.log("Classic Taxi")
+    }
+}
+ 
+function main(input) {
+    const data = input.split("\n");
+    solve(data)
+}
