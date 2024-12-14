13ccca03b0433db69c188e2a885c12975660110c
Ahmad Aldali <32056043+ahmadaldali@users.noreply.github.com>
2024-12-14 14:34:46 +0200

    Update The-Strongest-String-1.js

--- The-Strongest-String-1.js
+++ The-Strongest-String-1.js
@@ -1,1 +1,39 @@
-
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+var stdin_input = "";
+ 
+process.stdin.on("data", function (input) {
+    stdin_input += input;
+});
+ 
+process.stdin.on("end", function () {
+    main(stdin_input);
+});
+ 
+function solve(data) {
+    const N = parseInt(data[0]);
+    const S = data[1];
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+ 
+    let result = '';
+    let maxIndex = 0;
+ 
+    for (let i = alphabet.length - 1; i >= 0; i--) {
+        const char = alphabet[i];
+        let index = S.indexOf(char, maxIndex);
+ 
+        while (index !== -1) {
+            result += char;
+            maxIndex = index + 1;
+            index = S.indexOf(char, maxIndex);
+            break; 
+        }
+    }
+ 
+    console.log(result);
+}
+ 
+function main(input) {
+    const data = input.trim().split("\n");
+    solve(data);
+}
