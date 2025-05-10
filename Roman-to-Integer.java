0e7986806e418da95270b6ee9d895eb982dd392d
Ahmad Aldali <32056043+ahmadaldali@users.noreply.github.com>
2025-05-10 12:45:04 +0300

    Create Roman-to-Integer.java

---
+++ Roman-to-Integer.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> romans = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+        );
+
+        int result = 0;
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = romans.get(s.charAt(i));
+            result = result + (current < prev ? -current : current);
+            prev = current;
+        }
+
+        return result;
+    }
+}
